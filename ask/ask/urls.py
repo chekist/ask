0c306c1716e083d1224b7af54d79bec29cc8c43b
chekist <serhiopich@gmail.com>
2016-11-15 22:33:03 +0300

    Fuck!

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import patterns, include, url
 
-urlpatterns = [
-    url(r'^$', 'root'),
-    url(r'^login/', 'login'),
-    url(r'^signup/', 'signup'),
-    url(r'^question/(\d+)/', 'question'),
-    url(r'^ask/', 'ask'),
-    url(r'^popular/', 'popular'),
-    url(r'^new/', 'new')
-]
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/$', 'qa.views.test'),
+    url(r'^signup/$', 'qa.views.test'),
+    url(r'^question/(\d+)$', 'qa.views.test'),
+    url(r'^ask/.*$', 'qa.views.test'),
+    url(r'^popular/$', 'qa.views.test'),
+    url(r'^new/$', 'qa.views.test'),
+)
