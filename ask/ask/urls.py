41e88ee2d8ce9877c5dd2a3193cac67d4331b52d
chekist <serhiopich@gmail.com>
2016-11-15 20:41:46 +0300

    xc

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,4 +18,10 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
