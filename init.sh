b23f06460f181bbac5b290beaa2a64f64a11cac4
chekist <serhiopich@gmail.com>
2016-11-15 22:57:35 +0300

    Fuck!

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.ask /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
