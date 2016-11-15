43f5114d404da53eb198ef4dbfb16a16d88c435c
chekist <serhiopich@gmail.com>
2016-11-15 22:12:47 +0300

    xc

--- init.sh
+++ init.sh
@@ -6,3 +6,4 @@ sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 cd /home/box/web/
 sudo gunicorn -c /home/box/web/etc/hello.py hello:app
+gunicorn -c /home/box/web/etc/django.py wsgi --daemon
