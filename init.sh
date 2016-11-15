8195b6327c3b90e4e4e603f8cb91a439c6363a34
chekist <serhiopich@gmail.com>
2016-11-15 22:27:38 +0300

    Fuck!

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-/etc/init.d/nginx restart
+sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 
-gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
+sudo gunicorn -c /home/box/web/etc/django.py wsgi --daemon
