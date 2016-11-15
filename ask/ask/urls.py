e9473758e84b2f1f4303166223713cd293f75d63
chekist <serhiopich@gmail.com>
2016-11-15 22:30:17 +0300

    Fuck!

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', 'root',),
+    url(r'^/', 'root'),
     url(r'^login/', 'login'),
     url(r'^signup/', 'signup'),
     url(r'^question/(\d+)/', 'question'),
