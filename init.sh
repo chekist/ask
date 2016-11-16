ce62c3dace0e61aff6bca38f2fdb369ba358736e
chekist <serhiopich@gmail.com>
2016-11-16 16:17:15 +0300

    asd

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
-sudo ln   -sf   /home/box/web/etc/gunicorn.conf      /etc/gunicorn.d/ask
+sudo ln -s ~/web/hello.py   /etc/gunicorn.d/test
+sudo ln   -sf   ~/web/etc/gunicorn.conf      /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
-cd /home/box/web/
-sudo gunicorn -c /home/box/web/etc/ask.py wsgi --daemon
+cd ~/web/
+sudo gunicorn -c ~/web/etc/ask.py wsgi --daemon
