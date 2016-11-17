2566e63caa9cb99c0ddd78b83a620d9d5aa279fb
chekist <serhiopich@gmail.com>
2016-11-17 19:55:50 +0300

    d

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf ~/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf ~/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application
