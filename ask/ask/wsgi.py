d3611ceb44d09b76fef0b3fe7d0d15c978531f7c
chekist <serhiopich@gmail.com>
2016-11-17 19:59:26 +0300

    d

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -5,7 +5,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os, sys
+import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 
